0ddfead3fcaeae2af14ef0117f9a728d84e58beb
Wolfgang Mattuttis <wolfgang.mattuttis@sanoma.com>
2024-09-17 09:10:50 +0200

    Added lazy loaded manager module

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ManagerHomeComponent} from "./manager-home/manager-home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: ManagerHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ManagerRoutingModule { }
+export class ManagerRoutingModule {
+}
